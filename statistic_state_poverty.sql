cb1865a351470926fbfd1e86412b4cf58d7d0bbf
Oussama Bouhali <oussama.bouhali@ensi-uma.tn>
2026-09-14 00:47:43 +0100

    Add files via upload

---
+++ statistic_state_poverty.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 02 juin 2021 à 01:23
+-- Version du serveur :  10.4.19-MariaDB
+-- Version de PHP : 7.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `output_projet_mmed`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `statistic state poverty`
+--
+
+CREATE TABLE `statistic state poverty` (
+  `State` varchar(20) NOT NULL,
+  `Percent robberies` varchar(20) NOT NULL,
+  `Percent aggrravated` varchar(20) NOT NULL,
+  `Percent murders` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
